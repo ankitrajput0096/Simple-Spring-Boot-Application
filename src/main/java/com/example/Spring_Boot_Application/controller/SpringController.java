a639849d49a5e7a76bf14a107942ae855a4196c8
ankitrajput0096 <35539994+ankitrajput0096@users.noreply.github.com>
2018-01-20 23:20:47 +0530

    third commit

--- src/main/java/com/example/Spring_Boot_Application/controller/SpringController.java
+++ src/main/java/com/example/Spring_Boot_Application/controller/SpringController.java
@@ -1,14 +1,32 @@
 package com.example.Spring_Boot_Application.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Spring_Boot_Application.model.Topic;
 
 @RestController
 public class SpringController {
 	
-	@RequestMapping("/hello")
+	@RequestMapping(value="/hello")
 	public String helloMethod()
 	{
 		return "hello friends";
+	}
+	
+	@RequestMapping(value="/topics")
+	public List<Topic> listOfTopics()
+	{
+		List<Topic> topics=new ArrayList<>();
+		topics.add(new Topic("spring","springFramework","spring framework is awesome"));
+
+		topics.add(new Topic("java","core java","java is very best in it's field"));
+
+		topics.add(new Topic("javascript","basic javascript","javascript is not so good"));
+		
+		return topics;
 	}
 }
